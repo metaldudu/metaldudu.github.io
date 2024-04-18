aef8941e47e2b71b297c10eef71e8b3649d6436b
metaldudu <metaldudu@gmail.com>
2024-04-18 15:52:48 +0800

    “update”

--- get_aqi.py
+++ get_aqi.py
@@ -1,14 +1,15 @@
 import requests
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup 
 import html
 
-# 发送请求获取XML内容
-url = "http://218.11.10.130:8080/api/hour/130000.xml"
-response = requests.get(url)
-xml_content = response.content
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
 
-# 解析XML
-root = ET.fromstring(xml_content)
+linkurl = 'https://www.iqair.cn/cn/china/hebei/shijiazhuang/xinhua' #url对应空气监测站点
+air_site = linkurl.split('/')[-1] #获取唯一的站点名称
+
+res = requests.get(linkurl, headers=headers)
+soup = BeautifulSoup(res.content, 'html.parser')
 
 # 初始化HTML内容
 html_content = """
@@ -34,35 +35,35 @@ html_content = """
     </style>
 </head>
 <body>
-
+<h1>空气质量</h1>
 """
 
-# 查找符合条件的部分
-for pointer in root.findall(".//Pointer"):
-    name_element = pointer.find("Name")
-    if name_element is not None and name_element.text == "市区职工医院":
-        # 找到符合条件的节点
-        html_content += "<h1>市区职工医院空气情况：</h1>"
-        
-        # 提取<DataTime>和<AQI>的内容
-        data_time = pointer.find("DataTime").text
-        aqi = pointer.find("AQI").text
-        html_content += f"<p>DataTime: {html.escape(data_time)}</p>"
-        html_content += f"<p>AQI: {html.escape(aqi)}</p>"
-        
-        # 提取<Polls>下面<Name>为“PM2.5”和“PM10”的部分
-        polls = pointer.find("Polls")
-        for poll in polls.findall(".//Poll"):
-            name = poll.find("Name").text
-            value = poll.find("Value").text
-            if name == "PM2.5" or name == "PM10":
-                html_content += f"<p>{name} : {html.escape(value)}</p>"
+air_aqi = soup.find("p", class_="aqi-value__value").string #aqi数值
+    
+air_all = soup.find("table", class_="aqi-overview-detail__other-pollution-table") #空气质量table
+
+pm25 = air_all.find_all('tr')[1]  # 第二个tr
+pm25_num = pm25.find_all('td')[2].select('span')[0].string
+
+pm10 = air_all.find_all('tr')[2]
+pm10_num = pm10.find_all('td')[2].select('span')[0].string
+
+weather = soup.find("div", class_="weather") #天气部分
+temperature = weather.table.find_all('tr')[1].find_all('td')[1].string
+humidity = weather.table.find_all('tr')[2].find_all('td')[1].string
+
+html_content += f"<p>AQI : {html.escape(air_aqi)}</p>"
+html_content += f"<p>PM2.5 : {html.escape(pm25_num)}</p>"
+html_content += f"<p>PM10 : {html.escape(pm10_num)}</p>"
+html_content += f"<p>TMP : {html.escape(temperature)}</p>"
+html_content += f"<p>HUM : {html.escape(humidity)}</p>"
+
 
 # 添加HTML文件的结尾标签
 html_content += "</body></html>"
 
 # 将HTML内容写入文件
-with open('../../aqi.html', 'w', encoding='utf-8') as file:
+with open('aqi.html', 'w', encoding='utf-8') as file:
     file.write(html_content)
 
 print("HTML文件已生成。")
